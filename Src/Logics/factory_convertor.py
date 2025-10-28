a2d881c4e1bdd973b6bb41ab43886e13a29f1a0b
ilushenka <saharokbro@mail.ru>
2025-10-28 19:51:45 +0800

    delete repeating part

--- Src/Logics/factory_convertor.py
+++ Src/Logics/factory_convertor.py
@@ -17,15 +17,11 @@ class factory_convertor:
         self.__basic = basic_converter()
         self.__datetime = datetime_converter()
         self.__reference = reference_converter()
-        # Передаём фабрику (self) в structure_converter
         self.__structure = structure_converter(self)
 
     def create(self, obj):
         if obj is None:
             raise argument_exception("Невозможно конвертировать None")
-
-        if isinstance(obj, list):
-            return [self.create(item) for item in obj]
 
         if isinstance(obj, (int, float, str, bool)):
             return self.__basic.convert(obj)
